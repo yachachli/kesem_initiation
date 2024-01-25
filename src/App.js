93ce8afb47403c643fd99d93baaa8129e8a5a2d6
Yahli Hazan <yahel@robustintelligence.com>
2024-01-24 22:46:28 -0800

    accidentally deleted navigate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 import './App.css';
 import moment from 'moment-timezone';
-import { NavLink } from 'react-router-dom';
 
 
 
