d528d1eb8faa698ccf6c247768c654e32f4ed0bd
Yahli Hazan <yahel@robustintelligence.com>
2024-01-24 22:02:18 -0800

    forgot to add navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './App.css';
 import moment from 'moment-timezone';
 import { NavLink } from 'react-router-dom';
