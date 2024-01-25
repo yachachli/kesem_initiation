90b984b36b9c787167512adc0439ea853087125d
Yahli Hazan <yahel@robustintelligence.com>
2024-01-24 22:01:33 -0800

    new routing

--- src/App.js
+++ src/App.js
@@ -97,7 +97,8 @@ function App() {
 
         <Routes>
           <Route path="/scavenger-hunt-list" element={<ScavengerHuntList />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
         </Routes>
       </div>
     </Router>
